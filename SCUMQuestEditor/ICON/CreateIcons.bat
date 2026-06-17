30b2eaebffcb78f689fba4bac4c61e2fb186a767
ntoff <norahtihtzoff@gmail.com>
2026-06-17 13:12:36 +1000

    icon changes

---
+++ SCUMQuestEditor/ICON/CreateIcons.bat
@@ -0,0 +1,4 @@
+"C:\Program Files\Inkscape\bin\inkscape.exe" -w 16 -h 16 -o 16.png icon.svg
+"C:\Program Files\Inkscape\bin\inkscape.exe" -w 32 -h 32 -o 32.png icon.svg
+"C:\Program Files\Inkscape\bin\inkscape.exe" -w 48 -h 48 -o 48.png icon.svg
+magick 16.png 32.png 48.png icon.ico
