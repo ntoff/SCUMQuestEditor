2a152e69c1b1b8a16f79463b32f2dd802f72aa2d
ntoff <norahtihtzoff@gmail.com>
2026-07-19 21:36:03 +1000

    about dialog changes

--- SCUMQuestEditor/AboutDialog.xaml.cs
+++ SCUMQuestEditor/AboutDialog.xaml.cs
@@ -1,5 +1,6 @@
-using System.Globalization;
+using System.IO;
 using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace SCUMQuestEditor
 {
@@ -10,6 +11,22 @@ namespace SCUMQuestEditor
             InitializeComponent();
             LblVersion.Text = "Version 1.0.0";
             LblBuildDate.Text = $"Built on {BuildInfo.BuildDate}";
+            LoadIcon();
+        }
+
+        private void LoadIcon()
+        {
+            string iconPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ICON", "icon.ico");
+            if (File.Exists(iconPath))
+            {
+                var bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new System.Uri(iconPath, System.UriKind.Absolute);
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.EndInit();
+                bitmap.Freeze();
+                ImgIcon.Source = bitmap;
+            }
         }
 
         private void BtnOk_Click(object sender, RoutedEventArgs e)
