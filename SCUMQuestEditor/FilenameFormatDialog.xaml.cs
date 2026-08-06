694acee37c09cc5bde6374a3687bff91e25f2bc3
ntoff <norahtihtzoff@gmail.com>
2026-08-06 16:24:51 +1000

    remove unneeded code

--- SCUMQuestEditor/FilenameFormatDialog.xaml.cs
+++ SCUMQuestEditor/FilenameFormatDialog.xaml.cs
@@ -11,10 +11,6 @@ namespace SCUMQuestEditor
         {
             InitializeComponent();
             TxtFormat.Text = currentFormat;
-            /*LblPlaceholders.Text = "Available placeholders:";
-            LblTier.Text = "{tier} - Quest tier (1, 2, 3)";
-            LblTrader.Text = "{trader} - Trader code (AR, BK, BA, BT, DC, GG, HM, HT, MC)";
-            LblTitle.Text = "{title} - Quest title (spaces replaced with underscores)";*/
         }
 
         private void BtnOk_Click(object sender, RoutedEventArgs e)
