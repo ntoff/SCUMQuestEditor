68fceb43e273d7f74c9d0c94e151ab184ec9c1cd
ntoff <norahtihtzoff@gmail.com>
2026-09-01 15:55:55 +1000

    Revert "fix json preview error state on load of saved quest"
    This reverts commit a043a6338db178608aa72544d54405aabc97df7f.

--- SCUMQuestEditor/MainWindow.xaml.cs
+++ SCUMQuestEditor/MainWindow.xaml.cs
@@ -709,6 +709,7 @@ namespace SCUMQuestEditor
 
                     _currentFilePath = filePath;
                     UpdateControlsFromQuest(loadedQuest);
+                    TxtJson.Text = jsonContent;
                     CurrentTradeDeal = loadedQuest;
                 }
             }
@@ -839,15 +840,9 @@ namespace SCUMQuestEditor
 
             return format + ".json";
         }
+
         private void UpdateControlsFromQuest(TradeDeal quest)
         {
-            // Disable all change handlers that would trigger UpdateJsonPreview during load
-            CbNpc?.SelectionChanged -= CbNpc_SelectionChanged;
-            CbTier?.SelectionChanged -= CbTier_SelectionChanged;
-            TxtTitle?.TextChanged -= TxtInput_TextChanged;
-            TxtDescription?.TextChanged -= TxtInput_TextChanged;
-            TxtTimeLimit?.TextChanged -= TxtInput_TextChanged;
-
             if (CbNpc != null)
             {
                 bool npcFound = false;
@@ -855,7 +850,9 @@ namespace SCUMQuestEditor
                 {
                     if (item.Tag?.ToString() == quest.AssociatedNpc)
                     {
+                        CbNpc.SelectionChanged -= CbNpc_SelectionChanged;
                         CbNpc.SelectedItem = item;
+                        CbNpc.SelectionChanged += CbNpc_SelectionChanged;
                         npcFound = true;
                         break;
                     }
@@ -865,14 +862,26 @@ namespace SCUMQuestEditor
                 {
                     ComboBoxItem newItem = new ComboBoxItem { Content = quest.AssociatedNpc };
                     CbNpc.Items.Add(newItem);
+                    CbNpc.SelectionChanged -= CbNpc_SelectionChanged;
                     CbNpc.SelectedItem = newItem;
+                    CbNpc.SelectionChanged += CbNpc_SelectionChanged;
                 }
             }
+
+            TxtTitle.TextChanged -= TxtInput_TextChanged;
+            TxtDescription.TextChanged -= TxtInput_TextChanged;
+            TxtTimeLimit.TextChanged -= TxtInput_TextChanged;
+            CbTier.SelectionChanged -= CbTier_SelectionChanged;
 
             if (TxtTitle != null) TxtTitle.Text = quest.Title;
             CbTier!.SelectedIndex = quest.Tier - 1;
             if (TxtDescription != null) TxtDescription.Text = quest.Description;
             if (TxtTimeLimit != null) TxtTimeLimit.Text = quest.TimeLimitHours.ToString("0.0#");
+
+            TxtTitle!.TextChanged += TxtInput_TextChanged;
+            TxtDescription!.TextChanged += TxtInput_TextChanged;
+            TxtTimeLimit!.TextChanged += TxtInput_TextChanged;
+            CbTier.SelectionChanged += CbTier_SelectionChanged;
 
             if (quest.RewardPool != null && quest.RewardPool.Count > 0)
             {
@@ -918,6 +927,7 @@ namespace SCUMQuestEditor
                 if (LvTradeDeals != null) LvTradeDeals.ItemsSource = null;
             }
 
+            // --- FIX START ---
             // Clear the observable collection
             ConditionsList.Clear();
 
@@ -929,19 +939,13 @@ namespace SCUMQuestEditor
                     ConditionsList.Add(condition);
                 }
             }
+            // --- FIX END ---
 
             LvConditions.ItemsSource = null;
             LvConditions.ItemsSource = conditionsView;
             LvConditions.SelectedItem = null;
+            
 
-            // Re-enable change handlers after all data is loaded
-            CbNpc?.SelectionChanged += CbNpc_SelectionChanged;
-            CbTier?.SelectionChanged += CbTier_SelectionChanged;
-            TxtTitle?.TextChanged += TxtInput_TextChanged;
-            TxtDescription?.TextChanged += TxtInput_TextChanged;
-            TxtTimeLimit?.TextChanged += TxtInput_TextChanged;
-
-            UpdateJsonPreview();
         }
 
 
