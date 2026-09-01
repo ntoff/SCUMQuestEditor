0cd38d3697122a7618b96d077c100838387fd29a
ntoff <norahtihtzoff@gmail.com>
2026-09-02 01:38:40 +1000

    update updatebuildinfo scripts

--- SCUMQuestEditor/Scripts/UpdateBuildInfo.bat
+++ SCUMQuestEditor/Scripts/UpdateBuildInfo.bat
@@ -1,12 +1,12 @@
 @echo off
 set DATE_STR=%date:~-4,4%-%date:~-10,2%-%date:~-7,2% %time:~0,2%:%time:~3,2%:%time:~6,2%
-set DATE_STR=!DATE_STR: =0!
-for /f "delims=" %%V in (Version.txt) do set VERSION=%%V
-echo namespace SCUMQuestEditor > BuildInfo.cs
-echo { >> BuildInfo.cs
-echo     public static class BuildInfo >> BuildInfo.cs
-echo     { >> BuildInfo.cs
-echo         public const string BuildDate = "!!DATE_STR!!"; >> BuildInfo.cs
-echo         public const string Version = "!VERSION!"; >> BuildInfo.cs
-echo     } >> BuildInfo.cs
-echo } >> BuildInfo.cs
+set DATE_STR=!DATE_STR: !0!
+for /f "delims=" %%V in (%~dp0\..\Version.txt) do set VERSION=%%V
+echo namespace SCUMQuestEditor > %~dp0..\BuildInfo.cs
+echo { >> %~dp0..\BuildInfo.cs
+echo     public static class BuildInfo >> %~dp0..\BuildInfo.cs
+echo     { >> %~dp0..\BuildInfo.cs
+echo         public const string BuildDate = "!!DATE_STR!!"; >> %~dp0..\BuildInfo.cs
+echo         public const string Version = "!VERSION!"; >> %~dp0..\BuildInfo.cs
+echo     } >> %~dp0..\BuildInfo.cs
+echo } >> %~dp0..\BuildInfo.cs
--- SCUMQuestEditor/Scripts/UpdateBuildInfo.ps1
+++ SCUMQuestEditor/Scripts/UpdateBuildInfo.ps1
@@ -1,5 +1,6 @@
 $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-$version = [System.IO.File]::ReadAllText("Version.txt").Trim()
+$projectDir = Split-Path $PSScriptRoot -Parent
+$version = [System.IO.File]::ReadAllText((Join-Path $projectDir "Version.txt")).Trim()
 $quote = [char]34
 $cs = "namespace SCUMQuestEditor`r`n{`r`n    public static class BuildInfo`r`n    {`r`n        public const string BuildDate = $quote$date$quote;`r`n        public const string Version = $quote$version$quote;`r`n    }`r`n}"
-[System.IO.File]::WriteAllText("BuildInfo.cs", $cs, [System.Text.Encoding]::UTF8)
+[System.IO.File]::WriteAllText((Join-Path $projectDir "BuildInfo.cs"), $cs, [System.Text.Encoding]::UTF8)
