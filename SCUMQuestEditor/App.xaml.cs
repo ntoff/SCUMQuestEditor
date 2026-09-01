6e496ab7c7e34ef01a0bfee3f45c3f59f8e4fecc
ntoff <norahtihtzoff@gmail.com>
2026-09-01 14:05:44 +1000

    allow windows open with to send files to the editor

--- SCUMQuestEditor/App.xaml.cs
+++ SCUMQuestEditor/App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
 namespace SCUMQuestEditor
 {
@@ -9,6 +7,22 @@ namespace SCUMQuestEditor
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ShutdownMode = ShutdownMode.OnLastWindowClose;
+
+            string? filePath = null;
+            var args = Environment.GetCommandLineArgs();
+
+            if (args.Length > 1)
+            {
+                filePath = args[1];
+            }
+
+            var mainWindow = !string.IsNullOrEmpty(filePath) ? new MainWindow(filePath) : new MainWindow();
+            mainWindow.Show();
+            this.MainWindow = mainWindow;
+        }
     }
 
 }
